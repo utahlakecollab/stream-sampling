4f1a6a5ceede957dba38318742c8727607ffdfc1
Tod Robbins <todd.d.robbins@gmail.com>
2019-06-26 13:47:36 -0600

    added site map markers and data popups

--- js/map.js
+++ js/map.js
@@ -4,13 +4,31 @@ function init() {
   Tabletop.init({
     key: publicSpreadsheetUrl,
     callback: showInfo,
-    simpleSheet: true,
-    order: 'SiteID'
+    simpleSheet: true
   })
 }
 
 function showInfo(data, tabletop) {
   console.log(data)
+  window.data = data
+  data.forEach(({ SiteID, Site_Name, Region, Reach, Site_Type, Latitude, Longitude, Access_Notes_March, Access_Notes_July, Access_Notes_October }) => {
+    L.marker([Latitude, Longitude], {
+      // color: 'white'
+    }).addTo(mymap)
+      .bindPopup(
+        `
+          <p><strong>Site ID:</strong> ${SiteID}</p>
+          <p><strong>Reach</strong> ${Reach}</p>
+          <p><strong>Site Name:</strong> ${Site_Name}</p>
+          <p class="maps"><strong>Lat/Lon:</strong> <a href="https://www.google.com/maps/place/@${Latitude},${Longitude},19z/data=!3m1!1e3" target="_blank">${Latitude}, ${Longitude}</a></p>
+          <p><strong>Region:</strong> ${Region}</p>
+          <p><strong>Site Type:</strong> ${Site_Type}</p>
+          <p><strong>Access Notes (March):</strong> ${Access_Notes_March}</p>
+          <p><strong>Access Notes (July):</strong> ${Access_Notes_July}</p>
+          <p><strong>Access Notes (October):</strong> ${Access_Notes_October}</p>
+        `
+      )
+  })
 }
 
 window.addEventListener('DOMContentLoaded', init)
